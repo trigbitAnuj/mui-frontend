54aac6c811e9b178c30b1a8463d615ae20c3e80f
Anuj kumar <trigbit.anuj@gmail.com>
2023-05-09 18:00:37 +0530

    Update styled.tsx

--- pages/styled.tsx
+++ pages/styled.tsx
@@ -1,31 +1,24 @@
 import { theme } from "@/theme";
-import { useTheme } from "@emotion/react";
-import {
-  ThemeProvider,
-  createTheme,
-  styled,
-  alpha,
-} from "@mui/material/styles";
 
 import { Container, Slider, Box, SliderProps } from "@mui/material/";
 
-import Link from "next/link";
-import { TextField, TextFieldProps, Typography } from "@mui/material";
+import { Typography, CssBaseline } from "@mui/material";
 import React from "react";
+import {
+  MyStyledButton,
+  StyledBox,
+  StyledBoxComponent,
+  StyledButtonDemo,
+  StyledContainer,
+  StyledLinkDemo,
+  StyledTextField,
+  SuccessSlider,
+} from "@/components/styledComponent";
 
 // interface MyThemeComponentProps {
 //   color?: "primary" | "secondary";
 //   variant?: "normal" | "dashed";
 // }
-
-interface StyleContainerType {
-  borderColor: string;
-}
-
-interface StyledTextFieldType {
-  placeHolderColor: string;
-  borderColor: string;
-}
 
 // const customTheme = createTheme({
 //   palette: {
@@ -96,97 +89,7 @@ interface StyledTextFieldType {
 //   padding: theme.spacing(1),
 // }));
 
-const StyledButton = styled("button")(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  backgroundColor: theme.palette.primary.main,
-  padding: theme.spacing(1),
-  borderRadius: theme.shape.borderRadius,
-}));
-
-const StyledLink = styled(Link)(({ theme }) => ({
-  color: theme.palette.common.white,
-  backgroundColor: theme.palette.custom.light,
-  textDecoration: "none",
-  margin: theme.spacing(1),
-  padding: theme.spacing(1),
-}));
-
-const SuccessSlider = styled(Slider)<SliderProps>(({ theme }) => ({
-  width: 500,
-  color: theme.palette.primary.main,
-  "& .MuiSlider-thumb": {
-    "&:hover, &.Mui-focusVisible": {
-      boxShadow: `0px 2px 0px 8px ${alpha(theme.palette.primary.main, 0.5)}`,
-    },
-    "&.Mui-active": {
-      boxShadow: `0px 0px 0px 14px ${alpha(theme.palette.primary.main, 0.5)}`,
-    },
-  },
-}));
-
-const StyledTextField = styled(TextField, {
-  shouldForwardProp: (prop) => {
-    return prop !== "placeHolderColor" && prop !== "borderColor";
-  },
-})((prop: StyledTextFieldType) => ({
-  input: {
-    color: theme.palette.success.main,
-    "&::placeholder": {
-      fontStyle: "oblique",
-    },
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: prop.borderColor,
-      borderRadius: "5px",
-    },
-  },
-}));
-
-const StyledContainer = styled(Container, {
-  shouldForwardProp: (props) => {
-    return props !== "borderColor";
-  },
-})((prop: StyleContainerType) => ({
-  display: "flex",
-  borderColor: prop.borderColor,
-  borderRadius: "5px",
-  border: 1,
-  justifyContent: "space-between",
-  alignItems: "center",
-  margin: 1,
-  padding: 2,
-}));
-
-const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.light,
-  color: theme.palette.grey[900],
-
-  "@media (min-width:640px)": {
-    backgroundColor: theme.palette.primary.dark,
-    color: theme.palette.grey[100],
-  },
-  [`@media screen and (min-width: ${theme.breakpoints.values.md}px)`]: {
-    backgroundColor: theme.palette.secondary.light,
-    color: theme.palette.grey[900],
-  },
-}));
-
-const StyledBoxComponent = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.light,
-  color: theme.palette.grey[900],
-  padding: theme.spacing(2),
-  textAlign: "center",
-
-  "& .MuiBox-root": {
-    "& p": {
-      backgroundColor: theme.palette.error.dark,
-      color: theme.palette.grey[100],
-    },
-  },
-}));
-
-const Styled = () => {
+const Styled: React.FC = () => {
   const [text, setText] = React.useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -195,8 +98,9 @@ const Styled = () => {
 
   return (
     <>
-      <StyledButton>Styled Button</StyledButton>
-      <StyledLink href="/login">Button</StyledLink>
+      <CssBaseline />
+      <StyledButtonDemo>Styled Button</StyledButtonDemo>
+      <StyledLinkDemo href="/login">Button</StyledLinkDemo>
 
       {/* <MyThemeComponent>Styled div with theme</MyThemeComponent> */}
       {/* <StyledDiv sx={{ m: 1 }} color="secondary" variant="dashed">Primary</StyledDiv> */}
@@ -218,10 +122,10 @@ const Styled = () => {
         borderColor="red"
         value={text}
         onChange={handleChange}
-        fullWidth
         label="Text Area"
         sx={{
-          margin: theme.spacing(2, 1),
+          width: 500,
+          margin: theme.spacing(1, 2),
           fontFamily: theme.fontFamily.montserrat,
         }}
       />
@@ -236,6 +140,8 @@ const Styled = () => {
       <StyledBoxComponent>
         <Typography>Child component in Box</Typography>
       </StyledBoxComponent>
+
+      <MyStyledButton myBackgroundColor="red">MyStyleButton</MyStyledButton>
     </>
   );
 };
