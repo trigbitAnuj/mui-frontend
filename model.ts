02e94dfa866e47ab3cc3eab71df531b45383f996
Anuj kumar <trigbit.anuj@gmail.com>
2023-05-09 18:00:28 +0530

    Create model.ts

---
+++ model.ts
@@ -0,0 +1,15 @@
+import {Theme} from "@mui/material"
+
+export interface StyleContainerType {
+    borderColor: string;
+  }
+  
+ export  interface StyledTextFieldType {
+    placeHolderColor: string;
+    borderColor: string;
+    theme?:Theme
+  }
+ export interface StyledButtonType {
+    myBackgroundColor: string;
+  }
+  
