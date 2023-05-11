f102b6af2b14ba1dfe7f4988a133c5d7c4ccd08c
Anuj kumar <trigbit.anuj@gmail.com>
2023-05-11 18:11:39 +0530

    Create animation.tsx

---
+++ pages/animation.tsx
@@ -0,0 +1,11 @@
+import { AnimationComponent } from "@/components/animationComponent";
+
+const Animation = () => {
+  return (
+    <>
+      <AnimationComponent />
+    </>
+  );
+};
+
+export default Animation;
